e97f40b31f691db39043f285b4516f4f678b1ae6
Petar <peki.trifunovic@gmail.com>
2021-12-26 22:38:32 +0100

    Hash table updated to work with 64bit keys

--- source/ht.c
+++ source/ht.c
@@ -5,7 +5,7 @@
 
 #include "ht.h"
 
-#define WORD_BITSIZE __CHAR_BIT__ * sizeof(uint32_t)
+#define DOUBLE_WORD_BITSIZE __CHAR_BIT__ * sizeof(uint64_t)
 
 
 ht* ht_create(hash_func h_func, hash_func_secondary h_func_secondary, uint32_t h_capacity_pow) 
@@ -13,7 +13,7 @@ ht* ht_create(hash_func h_func, hash_func_secondary h_func_secondary, uint32_t h
     ht* created_ht = (ht*)malloc(sizeof(ht));
     created_ht->h_func = h_func;
     created_ht->h_func_secondary = h_func_secondary;
-    uint32_t h_capacity = 1u << h_capacity_pow; //add overflow check
+    uint64_t h_capacity = 1u << h_capacity_pow; //add overflow check
     created_ht->entries = (ht_entry**)malloc(/*1.2 **/ h_capacity * sizeof(ht_entry*));
     for(int i = 0; i < h_capacity; i++)
     {
@@ -25,14 +25,15 @@ ht* ht_create(hash_func h_func, hash_func_secondary h_func_secondary, uint32_t h
     created_ht->capacity = h_capacity;
 }
 
-ht_entry* ht_get_entry_and_prev(ht* h_table, uint32_t h_key, ht_entry** h_prev, uint32_t* h_hash)
+ht_entry* ht_get_entry_and_prev(ht* h_table, uint64_t h_key, ht_entry** h_prev, uint64_t* h_hash)
 {
-    uint32_t hash = h_table->h_func(h_key, h_table->capacity_pow);
-    uint32_t original_hash = hash;
+    uint64_t hash = h_table->h_func(h_key, h_table->capacity_pow);
+    //printf("Calculated hfunc\n");
+    uint64_t original_hash = hash;
     ht_entry* entry = h_table->entries[hash];
+    //printf("Inserted in table")
     *h_prev = NULL;
     int i = 1;
-
 
     while (entry->status != EMPTY && entry->key != h_key)
     {
@@ -50,16 +51,18 @@ ht_entry* ht_get_entry_and_prev(ht* h_table, uint32_t h_key, ht_entry** h_prev, 
     return entry;
 }
 
-void ht_insert(ht* h_table, uint32_t h_key, int h_value)
+void ht_insert(ht* h_table, uint64_t h_key, int h_value)
 {
     //add 'table full' check
-    uint32_t hash;
+    uint64_t hash;
     ht_entry** prev = (ht_entry**)malloc(sizeof(ht_entry*));
     ht_entry* entry = ht_get_entry_and_prev(h_table, h_key, prev, &hash);
+    //printf("Found prev\n");
     if (entry != NULL && entry->key == HT_EMPTY_KEY)
     {
         if(__sync_val_compare_and_swap(&entry->key, HT_EMPTY_KEY, h_key) != HT_EMPTY_KEY)
             return ht_insert(h_table, h_key, h_value);
+        //printf("Inserted in ht\n");
         if(*prev != NULL)
         {
             (*prev)->next = hash;
@@ -70,11 +73,11 @@ void ht_insert(ht* h_table, uint32_t h_key, int h_value)
     }
     else if (entry == NULL) 
     {
-        printf("Value %d with key %d could not be inserted due to bad secondary hash!\n", h_value, h_key);
+        printf("Value %d with key %ld could not be inserted due to bad secondary hash!\n", h_value, h_key);
     }
     else if (entry->key == h_key) 
     {
-        printf("Value %d with key %d colud not be inserted because a value with the same key already exists!\n", h_value, h_key);
+        printf("Value %d with key %ld colud not be inserted because a value with the same key already exists!\n", h_value, h_key);
     }
 }
 
@@ -84,7 +87,7 @@ void ht_print_state(ht* h_table)
     {
         printf("%d: ", i);
         if (h_table->entries[i]->status == OCCUPIED)
-            printf("key=%d, value=%d", h_table->entries[i]->key, h_table->entries[i]->value);
+            printf("key=%ld, value=%d", h_table->entries[i]->key, h_table->entries[i]->value);
         printf("\n");
     }
     printf("\n");
@@ -92,28 +95,29 @@ void ht_print_state(ht* h_table)
 
 
 /* -------Private methods-------- */
-void _update_entry(ht_entry* entry, uint32_t h_key, int h_value, int next)
+void _update_entry(ht_entry* entry, uint64_t h_key, int h_value, int next)
 {
     entry->key = h_key;
     entry->value = h_value;
     entry->next = next;
 }
 
-uint32_t _primary_hash_function_fib(uint32_t h_key, uint32_t pow_value) //insert reference "hash func for int"
+uint32_t _primary_hash_function_fib(uint64_t h_key, uint32_t pow_value) //insert reference "hash func for int"
 {
-    const uint32_t fib_const = 2654435769;
-    return (h_key * fib_const) >> (WORD_BITSIZE - pow_value);
+    const uint64_t fib_const = 11400714819323198485;
+    return (h_key * fib_const) >> (DOUBLE_WORD_BITSIZE - pow_value);
 }
 
-uint32_t _secondary_hash_function(uint32_t h_key, int iter, uint32_t capacity)
+uint32_t _secondary_hash_function(uint64_t h_key, int iter, uint64_t capacity)
 {
     return (h_key + iter * iter) & (capacity - 1);
 }
 
 void main()
 {
-    ht* table = ht_create(_primary_hash_function_fib, _secondary_hash_function, 6);
-    for (int i=0; i<64; i++)
+    ht* table = ht_create(_primary_hash_function_fib, _secondary_hash_function, 7);
+    //printf("Created\n");
+    for (int i=0; i<63; i++)
     {
         ht_insert(table, rand() % 2048, rand() % 64);
         //printf("Inserted!\n");
--- source/ht.h
+++ source/ht.h
@@ -12,19 +12,19 @@ typedef enum
 
 typedef struct 
 {
-    uint32_t key;
+    uint64_t key;
     int value;
     int next;
     ht_entry_status status; 
 } ht_entry;
 
-typedef uint32_t (*hash_func)(uint32_t key, uint32_t pow_value);
-typedef uint32_t (*hash_func_secondary)(uint32_t key, int iter, uint32_t capacity);
+typedef uint32_t (*hash_func)(uint64_t key, uint32_t pow_value);
+typedef uint32_t (*hash_func_secondary)(uint64_t key, int iter, uint64_t capacity);
 
 typedef struct 
 {
     uint32_t capacity_pow;
-    uint32_t capacity;
+    uint64_t capacity;
     hash_func h_func;
     hash_func_secondary h_func_secondary;
     ht_entry** entries;
@@ -32,6 +32,6 @@ typedef struct
 
 
 ht* ht_create(hash_func h_func, hash_func_secondary h_func_secondary, uint32_t h_capacity_pow);
-ht_entry* ht_get_entry_and_prev(ht* h_table, uint32_t h_key, ht_entry** h_prev, uint32_t* h_hash);
-void ht_insert(ht* h_table, uint32_t h_key, int h_value);
+ht_entry* ht_get_entry_and_prev(ht* h_table, uint64_t h_key, ht_entry** h_prev, uint64_t* h_hash);
+void ht_insert(ht* h_table, uint64_t h_key, int h_value);
 void ht_print_state(ht* h_table);
